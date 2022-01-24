3065d52b13316d549d31cb59bafef24a64b77e52
Idevaldo <inetomec@yahoo.com.br>
2022-01-24 08:17:50 -0300

    feat: :sparkles: Finishing Challenge

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -10,21 +10,30 @@ export class GamesRepository implements IGamesRepository {
 
   constructor() {
     this.repository = getRepository(Game);
-  }
+  };
 
   async findByTitleContaining(param: string): Promise<Game[]> {
+    // Complete usando query builder
     return this.repository
       .createQueryBuilder()
-      // Complete usando query builder
-  }
+      .where("title ILIKE :title", {title: `%${param}%`})
+      .getMany();
+  };
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query(`
+      SELECT COUNT(id)
+      FROM games;
+    `);
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
+    // Complete usando query builder
     return this.repository
       .createQueryBuilder()
-      // Complete usando query builder
-  }
-}
+      .relation(Game, "users")
+      .of(id)
+      .loadMany();
+  };
+};
